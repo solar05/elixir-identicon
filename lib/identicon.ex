1c6f94c1090d83a8cfd3703bd9c81df379c95739
solar05 <artem00298@gmail.com>
2019-12-25 19:02:33 +0300

    Add struct and initial functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,22 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
+
   @doc """
   Hello world.
 
