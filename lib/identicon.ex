52d151fdc0ac0bcc3bc6c1ad45bfd7cac14ffea5
solar05 <artem00298@gmail.com>
2019-12-25 19:30:23 +0300

    Add mirroring of grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -17,6 +18,17 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
     %Identicon.Image{image | color: {red, green, blue}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 
   @doc """
